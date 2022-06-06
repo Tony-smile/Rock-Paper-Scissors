b84b72beaae72758014412269883a2b651099bd9
Tony <tony@localhost.localdomain>
2022-06-06 11:44:24 +0000

    wrong folder

--- script.js
+++
@@ -1,6 +0,0 @@
-const toggleBar = document.getElementsByClassName("toggleBtb")[0]
-const navBarLinks = document.getElementsByClassName("nav-links")[0]
-
-toggleBar.addEventListener("click", ()=>{
-navBarLinks.classList.toggle("active")
-})
